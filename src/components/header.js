f4efbc49b211803db48467b29e28c84fa875b557
David Leuliette <david.leuliette@smartpension.co.uk>
2017-08-30 22:53:55 +0100

    Add our first component

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => {
+  return <Text>Albums</Text>;
+}
+
+// Make a component available for the entire app
+export default Header;

dcdfb33061e0db7db84100dd342c8d25c3df7ed9
David Leuliette <david.leuliette@smartpension.co.uk>
2017-08-31 23:35:08 +0100

    Refactor to Class Based Component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View style={styles.row}>
-      <Text>Albumss</Text>
-    </View>
-  );
+class AlbumList extends Component {
+  render() {
+    return (
+      <View style={styles.row}>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
